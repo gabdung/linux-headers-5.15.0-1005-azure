1c58cca98f24a19d0cb798b73534cd05cfdba970
gabdung <gabdung49@gmail.com>
2025-08-02 17:36:56 -0700

    Create adpp.js

---
+++ adpp.js
@@ -0,0 +1,80 @@
+Request URL:
+https://marketplace.visualstudio.com/_apis/public/gallery/vscode/formulahendry/code-runner/latest
+Request Method:
+OPTIONS
+Status Code:
+200 OK
+Remote Address:
+150.171.74.16:443
+Referrer Policy:
+strict-origin-when-cross-origin
+access-control-allow-headers:
+content-type,vscode-sessionid,x-market-client-id,x-market-user-id, authorization
+access-control-allow-methods:
+OPTIONS,GET,POST,PATCH,PUT,DELETE
+access-control-allow-origin:
+*
+access-control-expose-headers:
+ActivityId,X-TFS-Session,X-MS-ContinuationToken,X-VSS-GlobalMessage,ETag,Server,X-MSEdge-Ref,X-TFS-ProcessId,X-VSS-E2EID
+access-control-max-age:
+3600
+activityid:
+40c34486-32a2-4c4d-9f2a-90b400815a36
+content-length:
+0
+date:
+Sat, 02 Aug 2025 23:47:29 GMT
+p3p:
+CP="CAO DSP COR ADMa DEV CONo TELo CUR PSA PSD TAI IVDo OUR SAMi BUS DEM NAV STA UNI COM INT PHY ONL FIN PUR LOC CNT"
+strict-transport-security:
+max-age=31536000; includeSubDomains
+x-cache:
+CONFIG_NOCACHE
+x-content-type-options:
+nosniff
+x-msedge-ref:
+Ref A: 3E3BB8BEE987496C8AD8D24A21265367 Ref B: JNB231060414007 Ref C: 2025-08-02T23:47:29Z
+x-tfs-processid:
+ed90a10a-c884-4790-b7a0-f743e7eff2e2
+x-tfs-session:
+40c34486-32a2-4c4d-9f2a-90b400815a36
+x-vss-e2eid:
+40c34486-32a2-4c4d-9f2a-90b400815a36
+x-vss-senderdeploymentid:
+2663b13f-50e3-a655-a159-22f6f4725fab
+:authority:
+marketplace.visualstudio.com
+:method:
+OPTIONS
+:path:
+/_apis/public/gallery/vscode/formulahendry/code-runner/latest
+:scheme:
+https
+accept:
+*/*
+accept-encoding:
+gzip, deflate, br, zstd
+accept-language:
+en-US
+access-control-request-headers:
+content-type,vscode-sessionid,x-market-client-id,x-market-user-id
+access-control-request-method:
+GET
+origin:
+vscode-file://vscode-app
+priority:
+u=1, i
+sec-fetch-dest:
+empty
+sec-fetch-mode:
+cors
+sec-fetch-site:
+cross-site
+user-agent:
+VSCode 1.102.3 (Code)
+vscode-sessionid:
+7e1af069174ec5e096f08a6e1d5191430ebb970fc1b2141488038a1c356fb275
+x-market-client-id:
+VSCode 1.102.3
+x-market-user-id:
+2ae30ae6-3d12-43dc-8043-91a78f4e6539
